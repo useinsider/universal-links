83be38b0ea77c2ffa801cd15642e0b81743f4a31
Agil Asadi <newsma.95@gmail.com>
2021-11-18 20:02:52 +0300

    Create handle.swift

---
+++ handle.swift
@@ -0,0 +1,20 @@
+
+func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+    if userActivity.activityType == NSUserActivityTypeBrowsingWeb {
+        guard let encodedURL = userActivity.webpageURL else {
+            print("Unable to handle user activity: No URL provided")
+            return false
+        }
+        let task = URLSession.shared.dataTask(with: encodedURL, completionHandler: { (data, response, error) in
+            guard let resolvedURL = response?.url else {
+                print("Unable to handle URL: \(encodedURL.absoluteString)")
+                return
+            }
+            // Now you have the resolved URL that you can
+            // use to navigate somewhere in the app.
+            print(resolvedURL)
+        })
+        task.resume()
+    }
+    return true
+}
